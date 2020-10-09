c8544dcb93162423e5df4eea9d9f47a64be99dd4
Robel kebede <robelkebede44@gmail.com>
2020-10-09 22:06:49 +0300

    server and output table

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 
 app = Flask(__name__)
@@ -7,6 +7,19 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/handle_data",methods=["GET","POST"])
+def index2():
+    year = request.form['year']
+    atom = request.form['atm']
+    gas = request.form['gas']
+    clouds = request.form['clouds']
+    alt = request.form['alt']
+    study = request.form['study']
+    layer = request.form['layer']  
+    print([year,atom,gas,clouds,alt,study,layer])
+
+    return render_template("result.html")
 
 
 if __name__ == "__main__":
