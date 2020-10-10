36e91d7e31123b079801dccc3b0d203396902b8c
Robel kebede <robelkebede44@gmail.com>
2020-10-10 15:20:32 +0300

    true color

--- test.py
+++ test.py
@@ -14,25 +14,30 @@ def generate():
     #np.save("stud_data.npy",sample)
 
     y = [i for i in range(len(sample))]
+    return sample
     #plt.scatter(sample,y)
     #plt.show()
 
+def means():
+    X = np.load("stud_data.npy")
+    X = np.append(3,X)[:,np.newaxis]
 
-X = np.load("stud_data.npy")
-X = np.append(3,X)[:,np.newaxis]
+    kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
+    labels = np.array(kmeans.labels_)[:,np.newaxis]
+    stud_id = np.array([i for i in range(len(labels))])[:,np.newaxis]
 
-kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
-labels = np.array(kmeans.labels_)[:,np.newaxis]
-stud_id = np.array([i for i in range(len(labels))])[:,np.newaxis]
+    print(labels.shape)
+    print(stud_id.shape)
+    table = np.concatenate([stud_id,labels],axis=1)
 
-print(labels.shape)
-print(stud_id.shape)
-table = np.concatenate([stud_id,labels],axis=1)
-
-print(table.shape)
-print(table[139])
+    print(table.shape)
+    print(table[139])
 
 
-#print(to_table)
+y = generate()
+
+plt.hist(y)
+plt.show()
+
 
 
